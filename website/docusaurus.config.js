1f19a2023d8d8cfbebce5f606d6d4a3f0cd0e4a4
Roman Rädle <raedle@fb.com>
2021-06-11 14:27:53 -0700

    Update docusaurus.config.js with base url

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://raedle.github.io/my-doc/',
-  baseUrl: '/',
+  baseUrl: '/my-doc/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
